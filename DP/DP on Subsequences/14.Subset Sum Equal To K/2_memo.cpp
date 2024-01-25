ddc148c220e226812f9985e02a64d0d0d629a54a
beeplovsharma <beeplovsharma@gmail.com>
2024-01-26 00:30:20 +0530

    dp on subsequence

---
+++ DP/DP on Subsequences/14.Subset Sum Equal To K/2_memo.cpp
@@ -0,0 +1,33 @@
+// Saare Subsequences me kisi ek ka bhi sum "K" ke barabar aajaye to "True" return krdo
+// TC : O(N*K)
+// SC : O(N*K) + O(N) ~ (dp size stack + auxiliary stack)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool fun(int n, int k, vector<int> &arr, vector<vector<int>> &dp)
+{
+    if (k == 0)
+        return true;
+    if (n == 0)
+        return (k == arr[0]);
+    if (dp[n][k] != -1)
+        return dp[n][k];
+
+    bool take = false;
+    if (k >= arr[n])
+        take = fun(n - 1, k - arr[n], arr, dp);
+    int notTake = fun(n - 1, k, arr, dp);
+
+    return dp[n][k] = (take | notTake);
+}
+
+int main()
+{
+    vector<int> arr = {4, 3, 2, 1};
+    int n = arr.size();
+    int k = 4;
+    vector<vector<int>> dp(n + 1, vector<int>(k + 1, -1));
+    cout << fun(n - 1, k, arr, dp);
+    return 0;
+}
